2438a77f6a53c95c00933b946ab9fe1379ceb9fd
prafesar <blagoveschenskij@gmail.com>
2019-04-30 22:11:11 +0300

    fix babel.config dell brousers

--- babel.config.js
+++ babel.config.js
@@ -2,19 +2,8 @@ module.exports = {
   presets: [
     ['@babel/env', {
       targets: {
-        node: 'current',
-        firefox: '60',
-        chrome: '67',
-        safari: '11.1',
+        node: 'current'
       },
     }],
-  ],
-  plugins: [
-    [
-      '@babel/plugin-proposal-pipeline-operator',
-      {
-        proposal: 'minimal',
-      },
-    ],
-  ],
+  ]
 };
