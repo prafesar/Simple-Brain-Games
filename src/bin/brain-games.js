1dfac9586e90bd905be8d4965001fcb8ad15eea2
prafesar <blagoveschenskij@gmail.com>
2019-05-06 16:33:46 +0300

    update depend

--- src/bin/brain-games.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import getAnswer from '..';
-
-console.log('\nWelcome to the Brain Games!\n');
-
-const userName = getAnswer('May I have your name? ');
-
-console.log(`Hi ${userName}!`);
