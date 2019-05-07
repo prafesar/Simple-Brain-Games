559254cc71299965415b9cbd30abe59645961741
prafesar <blagoveschenskij@gmail.com>
2019-05-07 17:07:08 +0300

    made new game brain-progression

---
+++ src/games/brain-progression.js
@@ -0,0 +1,33 @@
+import playGame, {
+  greeting, getRandomInteger, makeIssue,
+} from '..';
+
+function makeProgression(stepsCount = 10) {
+  const progression = [];
+  const startPoint = getRandomInteger();
+  const step = getRandomInteger(2, 5);
+  const iter = (acc, currentItem) => {
+    if (acc.length === stepsCount) {
+      return acc;
+    }
+    return iter([...acc, currentItem], currentItem + step);
+  };
+  return iter(progression, startPoint);
+}
+
+function getIssueForProgression() {
+  const progression = makeProgression();
+  // get random item of progression and hide it
+  const itemToHide = getRandomInteger(0, progression.length - 1);
+  const answer = progression[itemToHide];
+  const newProgression = progression.slice();
+  newProgression[itemToHide] = '..';
+  const condition = newProgression.join(' ');
+  return makeIssue(condition, answer);
+}
+
+export default () => {
+  greeting();
+  console.log('What number is missing in the progression?\n');
+  return playGame(getIssueForProgression);
+};
