943a1a4f6d5237953d51fbfe024f5a30b88f3555
prafesar <blagoveschenskij@gmail.com>
2019-05-08 22:11:00 +0300

    refactor even.js: add func isEven

--- src/games/even.js
+++ src/games/even.js
@@ -2,13 +2,15 @@ import playGame, {
   greeting, getRandomInteger, makeIssue,
 } from '..';
 
+const isEven = number => number % 2 === 0;
+
 export default () => {
   greeting();
   console.log('Answer "yes" if number even otherwise answer "no".\n');
   return playGame(
     () => {
       const gamesQuestion = getRandomInteger();
-      const answer = gamesQuestion % 2 === 0 ? 'yes' : 'no';
+      const answer = isEven(gamesQuestion) ? 'yes' : 'no';
       return makeIssue(gamesQuestion, answer);
     },
   );
