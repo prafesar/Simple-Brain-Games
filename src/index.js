71549067f22dfceea85c4c9c58ba5d271b6ca69b
prafesar <blagoveschenskij@gmail.com>
2019-05-11 13:59:26 +0300

    style index.js: rename arg

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ const getAnswer = issue => issue.answer;
 
 const output = str => console.log(str);
 
-export default (f, rules = '', steps = 3) => {
+export default (getIssue, rules = '', steps = 3) => {
   output('\nWelcome to the Brain Games!');
   output(rules);
   const userName = getUserAnswer('May I have your name? ');
   output(`Hi, ${userName}!\n`);
 
   for (let i = 0; i < steps; i += 1) {
-    const issue = f();
+    const issue = getIssue();
     const question = getQuestion(issue);
     const answer = getAnswer(issue);
     const userAnswer = getUserAnswer(`Question: ${question} \nYour answer: `);
